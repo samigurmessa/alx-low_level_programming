7e39cda16028ff7f0156a6f4e0997bf855e91b4f
samigurmessa <samuelgurmesa@duck.com>
2022-04-12 15:26:00 -0700

    prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
-#ifndef _holberton_h_
-#define _holberton_h_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-int _putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -14,5 +14,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
-#endif
+#endif /* _MAIN_H_ */
